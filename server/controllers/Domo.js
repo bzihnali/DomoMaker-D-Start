ca6c476eebef8c3da85dabc2e3a3f921a006fdb9
bytenode <bz5167@rit.edu>
2025-04-11 14:28:21 -0400

    Domomaker D

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -2,20 +2,12 @@ const models = require('../models');
 const Domo = models.Domo;
 
 const makerPage = async (req, res) => {
-  try {
-    const query = {owner: req.session.account._id};
-    const docs = await Domo.find(query).select('name age').lean().exec();
-
-    return res.render('app', { domos: docs });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: 'Error retrieving domos!' });
-  }
+  return res.render('app');
 };
 
 const makeDomo = async (req, res) => {
   if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'Borth name and age are required! '});
+    return res.status(400).json({ error: 'Borth name and age are required! ' });
   }
 
   const domoData = {
@@ -27,17 +19,31 @@ const makeDomo = async (req, res) => {
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.json({ redirect: '/maker' });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
-      return res.status(400).json({ error: 'Domo already exists! '});
+      return res.status(400).json({ error: 'Domo already exists!' });
     }
-    return res.status(500).json({ error: 'An error occurred making domo! '});
+    return res.status(500).json({ error: 'An error occurred making domo!' });
   }
 }
+
+const getDomos = async (req, res) => {
+  try {
+    const query = { owner: req.session.account._id };
+    const docs = await Domo.find(query).select('name age').lean().exec();
+
+    return res.json({ domos: docs });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Error retrieving domos!' });
+  }
+};
+
 
 module.exports = {
   makerPage,
   makeDomo,
+  getDomos,
 };
